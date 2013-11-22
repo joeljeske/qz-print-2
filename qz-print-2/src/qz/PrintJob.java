6981cfdef9bdb251c0a979c472260e9d8ad0ae91
Thomas Hart <tom@thomashart.me>
2013-11-22 12:46:44 -0500

    Refactor repo dir to add lib sources and certs

---
+++ qz-print-2/src/qz/PrintJob.java
@@ -0,0 +1,172 @@
+/**
+ * @author Tres Finocchiaro
+ *
+ * Copyright (C) 2013 Tres Finocchiaro, QZ Industries
+ *
+ * IMPORTANT: This software is dual-licensed
+ *
+ * LGPL 2.1 This is free software. This software and source code are released
+ * under the "LGPL 2.1 License". A copy of this license should be distributed
+ * with this software. http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * QZ INDUSTRIES SOURCE CODE LICENSE This software and source code *may* instead
+ * be distributed under the "QZ Industries Source Code License", available by
+ * request ONLY. If source code for this project is to be made proprietary for
+ * an individual and/or a commercial entity, written permission via a copy of
+ * the "QZ Industries Source Code License" must be obtained first. If you've
+ * obtained a copy of the proprietary license, the terms and conditions of the
+ * license apply only to the licensee identified in the agreement. Only THEN may
+ * the LGPL 2.1 license be voided.
+ *
+ */
+package qz;
+
+import java.awt.print.PrinterJob;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+/**
+ * PrintJob will provide an object to hold an entire job. It should contain the 
+ * raw data for the job, the title of the job, and it's current state.
+ * 
+ * @author Thomas Hart
+ */
+public class PrintJob implements Runnable {
+    
+    private PrintJobState state = PrintJobState.STATE_CREATED;
+    private String title = "Print Job";
+    private ArrayList<PrintJobElement> data = new ArrayList<PrintJobElement>();;
+    private Boolean running = true;
+    private Integer updateDelay = 100;
+    private Printer printer;
+    
+    public void run() {
+        while(running) {
+            try {
+                Thread.sleep(updateDelay);
+            } catch (InterruptedException ex) {
+                LogIt.log(ex);
+                running = false;
+            }
+        }
+    }
+    
+    public void cancel() {
+        state = PrintJobState.STATE_CANCELLED;
+        running = false;
+    }
+    
+    public PrintJobState getJobState() {
+        return state;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void append(ByteArrayBuilder appendData, Charset charset) {
+        try {
+            PrintJobElement pje = new PrintJobElement(this, appendData, "RAW", charset);
+            data.add(pje);
+        }
+        catch(NullPointerException e) {
+            LogIt.log(e);
+        }
+    }
+    
+    public void appendImage(ByteArrayBuilder imagePath, Charset charset, String lang, Integer imageX, Integer imageY) {
+        
+        try {
+            PrintJobElement pje = new PrintJobElement(this, imagePath, "IMAGE", charset, lang, imageX, imageY);
+            data.add(pje);
+        }
+        catch(NullPointerException e) {
+            LogIt.log(e);
+        }
+    }
+    public void appendImage(ByteArrayBuilder imagePath, Charset charset, String lang, Integer dotDensity) {
+        
+        try {
+            PrintJobElement pje = new PrintJobElement(this, imagePath, "IMAGE", charset, lang, dotDensity);
+            data.add(pje);
+        }
+        catch(NullPointerException e) {
+            LogIt.log(e);
+        }
+    }
+
+    public void appendXML(ByteArrayBuilder url, Charset charset, String xmlTag) {
+        PrintJobElement pje = new PrintJobElement(this, url, "XML", charset, xmlTag);
+        data.add(pje);
+    }
+    
+    public void appendFile(ByteArrayBuilder url, Charset charset) {
+        PrintJobElement pje = new PrintJobElement(this, url, "FILE", charset);
+        data.add(pje);
+    }
+    
+    public void prepareJob() {
+        
+        state = PrintJobState.STATE_PROCESSING;
+        
+        ListIterator dataIterator = data.listIterator();
+        
+        while(dataIterator.hasNext()) {
+            try {
+                PrintJobElement pje = (PrintJobElement) dataIterator.next();
+                pje.prepare();
+            } catch (IOException ex) {
+                LogIt.log(ex);
+            }
+        }
+
+        state = PrintJobState.STATE_PROCESSED;
+        
+    }
+    
+    public void queue() {
+        state = PrintJobState.STATE_QUEUED;
+    }
+    
+    // Returns a string with the contents of the job data
+    // TODO: This is a debugging/testing function. Not needed for release
+    public String getInfo() {
+        
+        String jobInfo = "";
+        
+        ListIterator dataIterator = data.listIterator();
+        
+        while(dataIterator.hasNext()) {
+            PrintJobElement pje = (PrintJobElement) dataIterator.next();
+            ByteArrayBuilder bytes = pje.getData();
+            String info;
+            try {
+                info = new String(bytes.getByteArray(), pje.getCharset().name());
+                jobInfo += info;
+            } catch (UnsupportedEncodingException ex) {
+                LogIt.log(ex);
+            }
+        }
+        
+        return jobInfo;
+        
+    }
+    
+    // TODO: Stub for now.
+    public void print() {
+        state = PrintJobState.STATE_SENDING;
+        
+        //PrinterJob pj = PrinterJob.getPrinterJob();
+        //pj.setPrintable(printer);
+        
+        state = PrintJobState.STATE_COMPLETE;
+    }
+    
+    public void setPrinter(Printer printer) {
+        this.printer = printer;
+    }
+
+ }
---
+++ qz-print-2/src/qz/PrintSpooler.java
@@ -0,0 +1,198 @@
+/**
+ * @author Tres Finocchiaro
+ *
+ * Copyright (C) 2013 Tres Finocchiaro, QZ Industries
+ *
+ * IMPORTANT: This software is dual-licensed
+ *
+ * LGPL 2.1 This is free software. This software and source code are released
+ * under the "LGPL 2.1 License". A copy of this license should be distributed
+ * with this software. http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * QZ INDUSTRIES SOURCE CODE LICENSE This software and source code *may* instead
+ * be distributed under the "QZ Industries Source Code License", available by
+ * request ONLY. If source code for this project is to be made proprietary for
+ * an individual and/or a commercial entity, written permission via a copy of
+ * the "QZ Industries Source Code License" must be obtained first. If you've
+ * obtained a copy of the proprietary license, the terms and conditions of the
+ * license apply only to the licensee identified in the agreement. Only THEN may
+ * the LGPL 2.1 license be voided.
+ *
+ */
+
+package qz;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+/**
+ * The PrintSpooler will maintain a list of all print jobs and their status.
+ * It will also initiate threaded processes for preparing print jobs and sending
+ * them to the printer with the appropriate interface.
+ * 
+ * @author Thomas Hart
+ */
+public class PrintSpooler implements Runnable {
+    
+    public boolean running;
+    public Integer loopDelay;
+    public PrintJob currentJob;
+    public Thread currentJobThread;
+    
+    private String queueInfo = "";
+    private ArrayList<PrintJob> spool = new ArrayList<PrintJob>();
+    private ListIterator<PrintJob> spoolIterator;
+    private Printer currentPrinter;
+    private FilePrinter filePrinter;
+    
+    public void PrintSpooler() {
+        
+    }
+    
+    // The run loop will consistently check the spool List and call functions
+    // based on the state of each PrintJob
+    public void run() {
+        
+        LogIt.log("PrintSpooler started");
+        
+        // Initialize system variables
+        running = true;
+        filePrinter = new FilePrinter();
+        // Configurable variables
+        loopDelay = 1000;
+    
+        // Main loop - run every loopDelay milliseconds
+        while(running) {
+            try {
+                
+                queueInfo = "";
+                spoolIterator = spool.listIterator();
+                
+                if(spool.size() > 0) {
+                    
+                    while(spoolIterator.hasNext()) {
+
+                        Integer jobIndex = spoolIterator.nextIndex();
+                        PrintJob job = spoolIterator.next();
+                        PrintJobState jobState = job.getJobState();
+                        
+                        switch(jobState) {
+                            case STATE_PROCESSED:
+                                job.queue();
+                                break;
+                            case STATE_QUEUED:
+                                // if printer is available, print the job
+                                break;
+                            default:
+                                break;
+                        };
+                        
+                        queueInfo += "Job #" + jobIndex + " Title: " + 
+                                job.getTitle() + " State: " + 
+                                jobState.toString() + "\n";
+                        
+                    }
+                    
+                }
+                else {
+                    
+                }
+                
+                Thread.sleep(loopDelay);
+                
+            } catch (InterruptedException ex) {
+                LogIt.log(ex);
+                running = false;
+            }
+        }
+    }
+    
+    public void createJob() {
+        currentJob = new PrintJob();
+        currentJobThread = new Thread(currentJob);
+        currentJobThread.start();
+        spool.add(currentJob);
+    }
+    
+    public void append(ByteArrayBuilder data, Charset charset) {
+        if(currentJob == null) {
+            createJob();
+        }
+        
+        currentJob.append(data, charset);
+    }
+    /**
+     * Creates an image PrintJobElement and adds it to the current print job
+     */
+    public void appendImage(ByteArrayBuilder imagePath, Charset charset, String lang, Integer imageX, Integer imageY) {
+        if(currentJob == null) {
+            createJob();
+        }
+        
+        currentJob.appendImage(imagePath, charset, lang, imageX, imageY);
+    }
+
+    public void appendImage(ByteArrayBuilder imagePath, Charset charset, String lang, Integer dotDensity) {
+        if(currentJob == null) {
+            createJob();
+        }
+        
+        currentJob.appendImage(imagePath, charset, lang, dotDensity);
+    }
+    
+    public void appendXML(ByteArrayBuilder url, Charset charset, String xmlTag) {
+        if(currentJob == null) {
+            createJob();
+        }
+        
+        currentJob.appendXML(url, charset, xmlTag);
+    }
+
+    public void appendFile(ByteArrayBuilder url, Charset charset) {
+        if(currentJob == null) {
+            createJob();
+        }
+        
+        currentJob.appendFile(url, charset);
+    }
+    
+    public boolean print() {
+        if(currentJob == null) {
+            return false;
+        }
+        
+        //currentJob.setPrinter = currentPrinter;
+        currentJob.prepareJob();
+        currentJob = null;
+        return true;
+    }
+    
+    public void printToFile(String filePath) {
+        if(currentJob != null) {
+            //currentJob.setPrinter = filePrinter;
+            
+            filePrinter.setOutputPath(filePath);
+            currentJob.setPrinter(filePrinter);
+            
+            currentJob.prepareJob();
+            currentJob = null;
+        }
+    }
+    
+    public void cancelJob(Integer jobIndex) {
+        PrintJob job = spool.get(jobIndex);
+        job.cancel();
+        spool.set(jobIndex, job);
+    }
+    
+    public String getQueueInfo() {
+       return queueInfo;
+    }
+    
+    public String getJobInfo(Integer jobIndex) {
+        PrintJob job = spool.get(jobIndex);
+        return job.getInfo();
+    }
+    
+}
